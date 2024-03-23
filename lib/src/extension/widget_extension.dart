7c34bd5d04d91727225ddeed68b3d2c2b4d515c6
Wayaer <>
2024-03-23 11:26:24 +0800

    no message

--- lib/src/extension/widget_extension.dart
+++ lib/src/extension/widget_extension.dart
@@ -321,14 +321,15 @@ extension ExtensionWidget on Widget {
           semanticContainer: semanticContainer ?? true,
           child: this);
 
-  Positioned positioned(
-          {Key? key,
-          double? left,
-          double? top,
-          double? right,
-          double? bottom,
-          double? width,
-          double? height}) =>
+  Positioned positioned({
+    Key? key,
+    double? left,
+    double? top,
+    double? right,
+    double? bottom,
+    double? width,
+    double? height,
+  }) =>
       Positioned(
           key: key,
           left: left,
@@ -339,7 +340,11 @@ extension ExtensionWidget on Widget {
           height: height,
           child: this);
 
-  Center center({Key? key, double? widthFactor, double? heightFactor}) =>
+  Center center({
+    Key? key,
+    double? widthFactor,
+    double? heightFactor,
+  }) =>
       Center(
           key: key,
           widthFactor: widthFactor,
@@ -416,11 +421,12 @@ extension ExtensionWidget on Widget {
           decoration: decoration,
           child: this);
 
-  Align align(
-          {Key? key,
-          Alignment alignment = Alignment.center,
-          double? widthFactor,
-          double? heightFactor}) =>
+  Align align({
+    Key? key,
+    Alignment alignment = Alignment.center,
+    double? widthFactor,
+    double? heightFactor,
+  }) =>
       Align(
           key: key,
           alignment: alignment,
@@ -428,11 +434,12 @@ extension ExtensionWidget on Widget {
           heightFactor: heightFactor,
           child: this);
 
-  ClipRRect clipRRect(
-          {Key? key,
-          BorderRadius borderRadius = BorderRadius.zero,
-          CustomClipper<RRect>? clipper,
-          Clip clipBehavior = Clip.antiAlias}) =>
+  ClipRRect clipRRect({
+    Key? key,
+    BorderRadius borderRadius = BorderRadius.zero,
+    CustomClipper<RRect>? clipper,
+    Clip clipBehavior = Clip.antiAlias,
+  }) =>
       ClipRRect(
           key: key,
           borderRadius: borderRadius,
@@ -440,19 +447,21 @@ extension ExtensionWidget on Widget {
           clipBehavior: clipBehavior,
           child: this);
 
-  ClipOval clipOval(
-          {Key? key,
-          BorderRadius borderRadius = BorderRadius.zero,
-          CustomClipper<Rect>? clipper,
-          Clip clipBehavior = Clip.antiAlias}) =>
+  ClipOval clipOval({
+    Key? key,
+    BorderRadius borderRadius = BorderRadius.zero,
+    CustomClipper<Rect>? clipper,
+    Clip clipBehavior = Clip.antiAlias,
+  }) =>
       ClipOval(
           key: key, clipper: clipper, clipBehavior: clipBehavior, child: this);
 
-  ClipPath clipPath(
-          {Key? key,
-          BorderRadius borderRadius = BorderRadius.zero,
-          CustomClipper<Path>? clipper,
-          Clip clipBehavior = Clip.antiAlias}) =>
+  ClipPath clipPath({
+    Key? key,
+    BorderRadius borderRadius = BorderRadius.zero,
+    CustomClipper<Path>? clipper,
+    Clip clipBehavior = Clip.antiAlias,
+  }) =>
       ClipPath(
           key: key, clipper: clipper, clipBehavior: clipBehavior, child: this);
 
@@ -469,13 +478,14 @@ extension ExtensionWidget on Widget {
       LimitedBox(
           key: key, maxWidth: maxWidth, maxHeight: maxHeight, child: this);
 
-  OverflowBox overflowBox(
-          {Key? key,
-          Alignment alignment = Alignment.center,
-          double? minWidth,
-          double? maxWidth,
-          double? minHeight,
-          double? maxHeight}) =>
+  OverflowBox overflowBox({
+    Key? key,
+    Alignment alignment = Alignment.center,
+    double? minWidth,
+    double? maxWidth,
+    double? minHeight,
+    double? maxHeight,
+  }) =>
       OverflowBox(
           key: key,
           alignment: alignment,
@@ -501,11 +511,12 @@ extension ExtensionWidget on Widget {
   ConstrainedBox constrainedBox(BoxConstraints constraints, {Key? key}) =>
       ConstrainedBox(key: key, constraints: constraints, child: this);
 
-  UnconstrainedBox unconstrainedBox(
-          {Key? key,
-          TextDirection? textDirection,
-          Alignment alignment = Alignment.center,
-          Axis? constrainedAxis}) =>
+  UnconstrainedBox unconstrainedBox({
+    Key? key,
+    TextDirection? textDirection,
+    Alignment alignment = Alignment.center,
+    Axis? constrainedAxis,
+  }) =>
       UnconstrainedBox(
           key: key,
           textDirection: textDirection,
@@ -523,46 +534,47 @@ extension ExtensionWidget on Widget {
           {Key? key}) =>
       gestureDetector(onLongPress: onLongPress, key: key);
 
-  GestureDetector gestureDetector(
-          {Key? key,
-          GestureTapDownCallback? onTapDown,
-          GestureTapUpCallback? onTapUp,
-          GestureTapCallback? onTap,
-          GestureTapCancelCallback? onTapCancel,
-          GestureTapDownCallback? onSecondaryTapDown,
-          GestureTapUpCallback? onSecondaryTapUp,
-          GestureTapCancelCallback? onSecondaryTapCancel,
-          GestureTapCallback? onDoubleTap,
-          GestureLongPressCallback? onLongPress,
-          GestureLongPressStartCallback? onLongPressStart,
-          GestureLongPressMoveUpdateCallback? onLongPressMoveUpdate,
-          GestureLongPressUpCallback? onLongPressUp,
-          GestureLongPressEndCallback? onLongPressEnd,
-          GestureDragDownCallback? onVerticalDragDown,
-          GestureDragStartCallback? onVerticalDragStart,
-          GestureDragUpdateCallback? onVerticalDragUpdate,
-          GestureDragEndCallback? onVerticalDragEnd,
-          GestureDragCancelCallback? onVerticalDragCancel,
-          GestureDragDownCallback? onHorizontalDragDown,
-          GestureDragStartCallback? onHorizontalDragStart,
-          GestureDragUpdateCallback? onHorizontalDragUpdate,
-          GestureDragEndCallback? onHorizontalDragEnd,
-          GestureDragCancelCallback? onHorizontalDragCancel,
-          GestureForcePressStartCallback? onForcePressStart,
-          GestureForcePressPeakCallback? onForcePressPeak,
-          GestureForcePressUpdateCallback? onForcePressUpdate,
-          GestureForcePressEndCallback? onForcePressEnd,
-          GestureDragDownCallback? onPanDown,
-          GestureDragStartCallback? onPanStart,
-          GestureDragUpdateCallback? onPanUpdate,
-          GestureDragEndCallback? onPanEnd,
-          GestureDragCancelCallback? onPanCancel,
-          GestureScaleStartCallback? onScaleStart,
-          GestureScaleUpdateCallback? onScaleUpdate,
-          GestureScaleEndCallback? onScaleEnd,
-          HitTestBehavior? behavior,
-          bool excludeFromSemantics = false,
-          DragStartBehavior dragStartBehavior = DragStartBehavior.start}) =>
+  GestureDetector gestureDetector({
+    Key? key,
+    GestureTapDownCallback? onTapDown,
+    GestureTapUpCallback? onTapUp,
+    GestureTapCallback? onTap,
+    GestureTapCancelCallback? onTapCancel,
+    GestureTapDownCallback? onSecondaryTapDown,
+    GestureTapUpCallback? onSecondaryTapUp,
+    GestureTapCancelCallback? onSecondaryTapCancel,
+    GestureTapCallback? onDoubleTap,
+    GestureLongPressCallback? onLongPress,
+    GestureLongPressStartCallback? onLongPressStart,
+    GestureLongPressMoveUpdateCallback? onLongPressMoveUpdate,
+    GestureLongPressUpCallback? onLongPressUp,
+    GestureLongPressEndCallback? onLongPressEnd,
+    GestureDragDownCallback? onVerticalDragDown,
+    GestureDragStartCallback? onVerticalDragStart,
+    GestureDragUpdateCallback? onVerticalDragUpdate,
+    GestureDragEndCallback? onVerticalDragEnd,
+    GestureDragCancelCallback? onVerticalDragCancel,
+    GestureDragDownCallback? onHorizontalDragDown,
+    GestureDragStartCallback? onHorizontalDragStart,
+    GestureDragUpdateCallback? onHorizontalDragUpdate,
+    GestureDragEndCallback? onHorizontalDragEnd,
+    GestureDragCancelCallback? onHorizontalDragCancel,
+    GestureForcePressStartCallback? onForcePressStart,
+    GestureForcePressPeakCallback? onForcePressPeak,
+    GestureForcePressUpdateCallback? onForcePressUpdate,
+    GestureForcePressEndCallback? onForcePressEnd,
+    GestureDragDownCallback? onPanDown,
+    GestureDragStartCallback? onPanStart,
+    GestureDragUpdateCallback? onPanUpdate,
+    GestureDragEndCallback? onPanEnd,
+    GestureDragCancelCallback? onPanCancel,
+    GestureScaleStartCallback? onScaleStart,
+    GestureScaleUpdateCallback? onScaleUpdate,
+    GestureScaleEndCallback? onScaleEnd,
+    HitTestBehavior? behavior,
+    bool excludeFromSemantics = false,
+    DragStartBehavior dragStartBehavior = DragStartBehavior.start,
+  }) =>
       GestureDetector(
           key: key,
           onTapDown: onTapDown,
