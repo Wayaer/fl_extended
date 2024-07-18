0f0e49d7de495b3492a4ce5638a6916a7a3fb04d
Wayaer <>
2024-07-18 17:34:55 +0800

    no message

--- lib/fl_extended.dart
+++ lib/fl_extended.dart
@@ -13,6 +13,7 @@ export 'src/components/animation.dart';
 export 'src/components/unified_button.dart';
 export 'src/components/icon_box.dart';
 export 'src/components/fl_inherited.dart';
+export 'src/components/keep_alive_wrapper.dart';
 
 /// const
 export 'src/const/typedef.dart';
